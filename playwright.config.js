b644da1737e6d5c97b39aaa36363703f0b4d4869
manatolym <ufamedvedev@gmail.com>
2025-11-11 19:06:07 +0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -24,12 +24,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'],
-    ['html', {
-      outputFolder: 'playwright-report',
-      open: 'never'
-    }]
-  ],
+  ['list'],
+  ['allure-playwright', { outputFolder: 'allure-results' }]
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   outputDir: 'allure-results',
   use: {
