7cb00415a9a94db75bca38fcb22518d3a8b92bb7
Anatolii Medvedev <Anatol.Medvedev@x5.ru>
2025-11-12 06:32:23 +0300

    Diploma v6

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 
@@ -8,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
+
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
