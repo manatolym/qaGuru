032cba0b13ce9ffbcaaf8ba6f809eca179419f30
Anatolii Medvedev <Anatol.Medvedev@x5.ru>
2025-11-13 04:55:02 +0300

    Diploma fin v2

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,4 +1,4 @@
-import { expect } from '@playwright/test'
+
 
 export class MainPage {
     constructor(page) {
--- src/todos.service.js
+++ src/todos.service.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 let URL = 'https://apichallenges.herokuapp.com/';
 
 export class TodosService {
